30c148e5457013e2c3ee4ed448a93917b82cf815
jeremy.petit-jean <jeremy.petit-jean@tech-advantage.com>
2019-07-18 12:03:08 +0200

    CountDiv 100% o(1)

--- src/main/java/com/codility/Exercies/prefixsums/countdiv/Solution.java
+++ src/main/java/com/codility/Exercies/prefixsums/countdiv/Solution.java
@@ -3,9 +3,6 @@ package com.codility.Exercies.prefixsums.countdiv;
 public class Solution {
 
 	public int solution(int A, int B, int K) {
-		System.out.println(((double) B - (double) A + 1d ) / (double) K);
-		if(A == B) return (A / K);
-		else if (K == A || K == B) return (int) (Math.round(((double) B - (double) A + 1d ) / (double) K)) + 1;
-		else return (int) (Math.round(((double) B - (double) A + 1d ) / (double) K));
+		return (B/K) - (A/K) + (A%K == 0 ? 1 : 0);
 	}
 }
