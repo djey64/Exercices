88570fe5279d8bac6a2ce3c71921f1a89a839103
jeremypetit-jean <jeremypetit-jean@192.168.1.150>
2021-09-13 22:33:32 +0200

    Leetcode ex3: Longest Substring Without Repeating Characters

---
+++ src/main/java/com/leetcode/E3LonguestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,39 @@
+package com.leetcode;
+
+import java.util.HashSet;
+
+
+/**
+ * Given a string s, find the length of the longest substring without repeating characters.
+ * 
+ * See more here: 
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ * 
+ * @author jeremypetit-jean
+ *
+ */
+public class E3LonguestSubstringWithoutRepeatingCharacters {
+
+    public int lengthOfLongestSubstring(String s) {
+    	HashSet<String> subString = new HashSet<>();
+    	int maxLength = 0;
+    	
+    	int start = 0;
+    	int end = 0;
+    	
+    	while(end < s.length()) {
+    		char cEnd = s.charAt(end);
+    		if(subString.contains(String.valueOf(cEnd))) {
+    			while(subString.contains(String.valueOf(cEnd))) {
+    	    		char cStart = s.charAt(start);
+    				subString.remove(String.valueOf(cStart));
+    				start++;
+    			}
+    		} 
+			subString.add(String.valueOf(cEnd));
+			maxLength = Math.max(maxLength, subString.size());
+    		end++;
+    	}
+    	return maxLength;
+    }	
+}
